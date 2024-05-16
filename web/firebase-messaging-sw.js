665ead2549baf6078d2c7afe3a18e1a81343658d
tinnguyendh <nguyentindh2002@gmail.com>
2024-05-16 22:32:11 +0700

    delete some file

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts(
 );
 
 firebase.initializeApp({
-    apiKey: "AIzaSyC2cn9k4bOJ5TGRNFlj-XSkKdDPrKC8gEY",
-    authDomain: "insta-d9db8.firebaseapp.com",
-    projectId: "insta-d9db8",
-    storageBucket: "insta-d9db8.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "944785097371",
-    appId: "1:944785097371:web:864ed0e5ae67bbd1b43c3e",
-    measurementId: "G-9WSD8HL1FQ",
+    appId: "",
+    measurementId: "",
 });
 
 const messaging = firebase.messaging();
